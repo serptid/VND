227154b75c48f98d8adcb787cc84d522af545b1a
noth-an <ulko_2006@mail.ru>
2025-05-24 17:33:00 +1000

    first commit

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -4,22 +4,12 @@
 import { useEffect, useState } from 'react'
 
 export default function HomePage() {
-  const [message, setMessage] = useState('Загрузка...')
 
-  useEffect(() => {
-    fetch('http://localhost:8000')
-      .then((res) => res.json())
-      .then((data) => setMessage(data.message))
-      .catch((err) => {
-        console.error('Ошибка запроса:', err)
-        setMessage('Ошибка при подключении к API')
-      })
-  }, [])
 
   return (
     <div>
       <h2>Ответ от FastAPI:</h2>
-      <p>{message}</p>
+      <p></p>
     </div>
   )
 }
