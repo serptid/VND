e40af2320c6c62bc6e89338ee2429008ac5f1c8d
Сергей Прокопенко <psi200523@gmail.com>
2025-05-28 00:23:12 +1000

    httpsDel

--- front/src/app/components/SearchBox.tsx
+++ front/src/app/components/SearchBox.tsx
@@ -26,7 +26,7 @@ export default function SearchBar({ onSearch, fullDataList }: SearchBarProps) {
     }
 
     const timeout = setTimeout(() => {
-      fetch(`https://fefu.online:8000/abbreviations/search?query=${encodeURIComponent(query)}`)
+        fetch(`/api/abbreviations/search?query=${encodeURIComponent(query)}`)
         .then((res) => res.json())
         .then((data: Abbreviation[]) => {
           setSuggestions(data.slice(0, 7))
--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -16,7 +16,7 @@ export default function HomePage() {
   const [dataList, setDataList] = useState<Abbreviation[]>([])
 
   useEffect(() => {
-    fetch('https://fefu.online:8000/abbreviations/')
+    fetch('/api/abbreviations/')
       .then((res) => res.json())
       .then((data) => setDataList(data))
       .catch((err) => console.error('Ошибка при загрузке:', err))
