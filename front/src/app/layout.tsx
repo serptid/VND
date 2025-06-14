c0ee63a3e02cdd38d4e2a624d9d9efd55579c529
Сергей Прокопенко <psi200523@gmail.com>
2025-06-14 20:53:02 +1000

    healthcheck

--- front/src/app/layout.tsx
+++ front/src/app/layout.tsx
@@ -6,7 +6,7 @@ import Header from "@/app/components/Header"
 
 export const metadata = {
   title: 'ДВФУ - Аббревиатуры',
-  description: 'Создал Улько Данила',
+  description: 'Создал Прокопенко Сергей и Данила Улько - ДВФУ-2025',
 }
 
 export default function RootLayout({ children }: { children: ReactNode }) {
